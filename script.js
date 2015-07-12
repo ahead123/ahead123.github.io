344c49f9787fbb6ffd9ecfab5d2a6b2e1d794b55
Arthur Head <ahead747@gmail.com>
2015-07-12 14:11:57 -0500

    added contact form functionality

--- script.js
+++ script.js
@@ -20,11 +20,29 @@ $(document).ready(function(){
   // submit contact form and show message
   $('#submit').click(function(e){
     e.preventDefault();
-    $('div#contact').find('form').hide();
-    var name = $('div#contact').find('form input#name').val();
-    $('div#contact').css('height', '300px');
-    $('#mail-greeting').text(name);
-    $('#thanks').show('slow');
+    var $message = $('form input:nth-of-type(4)');
+    // show error if no message
+    if($message.val() === "Message" || $message.val() === ""){
+      $message.val("please enter a message before submitting").css("color", "red");
+        $message.click(function(){
+          $message.val("");
+          $message.css("color", "#fff");
+        });
+    }else{
+      // display thank you message with persons name
+      $('div#contact').find('form').hide();
+      var name = $('div#contact').find('form input#name').val();
+      $('div#contact').css('height', '300px');
+      $('#mail-greeting').text(name);
+      $('#thanks').show('slow');
+    }
+  });
+
+  // clear contact inputs on click
+  $('form input').click(function(){
+    if($(this).val() === "Phone_number" || $(this).val() === "Name" || $(this).val() === "Email" || $(this).val() === "Message"){
+      $(this).val("");
+    }  
   });
 
 
