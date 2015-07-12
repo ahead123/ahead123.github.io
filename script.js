4b26051adb56d5c35e57e59eefb3b3ec81a12001
Arthur Head <ahead747@gmail.com>
2015-07-12 13:28:24 -0500

    repushing changes

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+$(document).ready(function(){
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+  // jquery smooth scrolling links
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
+
+
+  // submit contact form and show message
+  $('#submit').click(function(e){
+    e.preventDefault();
+    $('div#contact').find('form').hide();
+    var name = $('div#contact').find('form input#name').val();
+    $('div#contact').css('height', '300px');
+    $('#mail-greeting').text(name);
+    $('#thanks').show('slow');
+  });
+
+
 });
